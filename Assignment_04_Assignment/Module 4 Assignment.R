9c1d8cb2b739baa99b8deb6dc1b7035543fa9a3f
VLiebrecht <155465457+VLiebrecht@users.noreply.github.com>
2025-09-19 13:10:25 -0500

    Create Module 4 Assignment.R

---
+++ Assignment_04_Assignment/Module 4 Assignment.R
@@ -0,0 +1,62 @@
+#Defining Vectors
+Frequency     <- c(0.6, 0.3, 0.4, 0.4, 0.2, 0.6, 0.3, 0.4, 0.9, 0.2)
+BloodPressure <- c(103, 87, 32, 42, 59, 109, 78, 205, 135, 176)
+FirstAssess   <- c(1, 1, 1, 1, 0, 0, 0, 0, NA, 1)    # bad=1, good=0
+SecondAssess  <- c(0, 0, 1, 1, 0, 0, 1, 1, 1, 1)    # low=0, high=1
+FinalDecision <- c(0, 1, 0, 1, 0, 1, 0, 1, 1, 1)    # low=0, high=1
+
+#Turn into dataframe
+df_hosp <- data.frame(
+  Frequency, BloodPressure, FirstAssess,
+  SecondAssess, FinalDecision, stringsAsFactors = FALSE
+)
+
+# Inspect
+summary(df_hosp)
+
+#Remove row with NA
+df_hosp <- na.omit(df_hosp)
+summary(df_hosp)
+
+##Visualizations
+# Boxplot of Blood Pressure: First Assessment
+boxplot(
+  BloodPressure ~ FirstAssess,
+  data = df_hosp,
+  names = c("Good","Bad"),
+  ylab = "Blood Pressure",
+  main = "BP by First MD Assessment"
+)
+
+# Boxplot of Blood Pressure: Second Assessment
+boxplot(
+  BloodPressure ~ SecondAssess,
+  data = df_hosp,
+  names = c("Low","High"),
+  ylab = "Blood Pressure",
+  main = "BP by Second MD Assessment"
+)
+
+# Boxplot of Blood Pressure: Final Decision
+boxplot(
+  BloodPressure ~ FinalDecision,
+  data = df_hosp,
+  names = c("Low","High"),
+  ylab = "Blood Pressure",
+  main = "BP by Final Decision"
+)
+
+# Histogram of Frequency Distribution
+hist(
+  df_hosp$Frequency,
+  breaks = seq(0, 1, by = 0.1),
+  xlab = "Visit Frequency",
+  main = "Histogram of Visit Frequency"
+)
+
+# Histogram of Blood Pressure Distribution
+hist(
+  df_hosp$BloodPressure,
+  breaks = 8,
+  xlab = "Blood Pressure",
+  main = "Histogram of Blood Pressure")
